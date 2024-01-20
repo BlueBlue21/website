fc9be5c400d76e6d508fc82c14aa08a697be70e9
BlueBlue21 <49936395+BlueBlue21@users.noreply.github.com>
2024-01-20 14:37:16 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# My Website 📘
+
+Made with Typescript based React and Million.js(SO FAST!), Chakra UI
+
+SORRY MY ENGLISH VERY SUCKSS...
+
+```bash
+# First Setup
+npm install
+npm run dev
+
+# Dev
+npm run dev
+
+# Build and Start
+npm run build
+npm run start
+```
---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import million from "million/compiler";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+	plugins: [million.vite({ auto: true }), react()],
+	server: {
+		host: "0.0.0.0",
+	},
+});
